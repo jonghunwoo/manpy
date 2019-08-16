3611a30fcf7111ad9fb71a7480d6f8af8b33234b
woojonghun <jonghun.digitalfactory@gmail.com>
2019-08-16 19:14:24 +0900

    modify AssemblyLine.py

--- AssemblyLine.py
+++ AssemblyLine.py
@@ -5,15 +5,13 @@ from dream.simulation.imports import Machine, Source, Exit, Part, Repairman,Queu
 
 #define the objects of the model
 Frame.capacity=4
-Sp=Source('SP','Parts', interArrivalTime={'Fixed':{'mean':0.5}},
-entity='Dream.Part')
-Sf=Source('SF','Frames', interArrivalTime={'Fixed':{'mean':2}},
-entity='Dream.Frame')
-M=Machine('M','Machine', processingTime={'Fixed':{'mean':0.25}})
+Sp=Source('SP','Parts', interArrivalTime={'Fixed':{'mean':0.5}}, entity='Dream.Part')
+Sf=Source('SF','Frames', interArrivalTime={'Fixed':{'mean':2}}, entity='Dream.Frame')
+M=Machine('M','Machine', processingTime={'Fixed':{'mean':1}})
 A=Assembly('A','Assembly', processingTime={'Fixed':{'mean':2}})
 E=Exit('E1','Exit')
-F=Failure(victim=M,
-distribution={'TTF':{'Fixed':{'mean':60.0}},'TTR':{'Fixed':{'mean':5.0}}})
+
+F=Failure(victim=M, distribution={'TTF':{'Fixed':{'mean':60.0}},'TTR':{'Fixed':{'mean':5.0}}})
 
 #define predecessors and successors for the objects
 Sp.defineRouting([A])
@@ -21,6 +19,7 @@ Sf.defineRouting([A])
 A.defineRouting([Sp,Sf],[M])
 M.defineRouting([A],[E])
 E.defineRouting([M])
+
 def main(test=0):
     # add all the objects in a list
     objectList=[Sp,Sf,M,A,E,F]
@@ -32,7 +31,8 @@ def main(test=0):
     runSimulation(objectList, maxSimTime)
 
     # calculate metrics
-    working_ratio=(A.totalWorkingTime/maxSimTime)*100
+    working_ratio_A = (A.totalWorkingTime / maxSimTime) * 100
+    working_ratio_M = (M.totalWorkingTime / maxSimTime) * 100
 
     # return results for the test
     if test:
@@ -40,7 +40,8 @@ def main(test=0):
 
     # print the results
     print "the system produced", E.numOfExits, "frames"
-    print "the working ratio of", A.objName, "is", working_ratio, "%"
+    print "the working ratio of", A.objName, "is", working_ratio_A, "%"
+    print "the working ratio of", M.objName, "is", working_ratio_M, "%"
 
 if __name__ == '__main__':
     main()
