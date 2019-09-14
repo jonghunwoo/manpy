95a130735820adff96ae8be0d36d7e5a6d74dae5
woojonghun <jonghun.digitalfactory@gmail.com>
2019-09-14 15:33:37 +0900

    Add a repeated shift

--- ServerWithShift.py
+++ ServerWithShift.py
@@ -6,7 +6,16 @@ S = Source('S1', 'Source', interArrivalTime={'Fixed': {'mean': 0.5}}, entity='Dr
 M = Machine('M1', 'Machine', processingTime={'Fixed': {'mean': 3}})
 E = Exit('E1', 'Exit')
 
-SS = ShiftScheduler(victim=M, shiftPattern=[[0, 5], [10, 15]])
+# create a repeated shift pattern
+shiftPattern = []
+i = 0
+while i < 100:
+    shiftPattern.append([i, i + 5])
+    i += 10
+print shiftPattern
+
+# create the shift
+SS = ShiftScheduler(victim=M, shiftPattern=shiftPattern)
 
 # define predecessors and successors for the objects
 S.defineRouting(successorList=[M])
@@ -18,7 +27,7 @@ def main(test=0):
     # add all the objects in a list
     objectList = [S, M, E, SS]
     # set the length of the experiment
-    maxSimTime = 20.0
+    maxSimTime = 100.0
     # call the runSimulation giving the objects and the length of the experiment
     runSimulation(objectList, maxSimTime)
 
